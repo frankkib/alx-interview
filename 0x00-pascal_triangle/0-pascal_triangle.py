ac57f819f87d6b514009b5a63cc44ead2ad19586
frankkib <frankkibagendi13@gmail.com>
2023-07-27 14:01:08 +0000

    this the pascal trangle interview question

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""""pascal triangle """
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    while len(triangle) < n:
+        prev_row = triangle[-1]
+        new_row = [1]
+
+        for i in range(1, len(prev_row)):
+            new_row.append(prev_row[i - 1] + prev_row[i])
+
+        new_row.append(1)
+        triangle.append(new_row)
+
+    return triangle
+if __name__ == "__main__":
+    pascal_tringale()
