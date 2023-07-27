3ed2ebfb10a329256cc925081b75fb6a21c29366
frankkib <frankkibagendi13@gmail.com>
2023-07-27 14:09:19 +0000

    this the pascal trangle interview question

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """"pascal triangle """
 def pascal_triangle(n):
+    """"checking if n is zero or negative"""
     if n <= 0:
         return []
+    """the top row"""
 
     triangle = [[1]]
     while len(triangle) < n:
