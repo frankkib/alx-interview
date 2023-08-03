857d2ed9fabf8ffc1b2d912cac8a336e1e23eda2
frankkib <frankkibagendi13@gmail.com>
2023-08-03 11:45:58 +0000

    funtion for dertemining which boxes were open

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""function to try and unlocked boxes"""
+from collections import deque
+
+
+def canUnlockAll(boxes):
+    """function to determine which boxes were open"""
+    n = len(boxes)
+    visited = set()
+    queue = deque([0])
+
+    while queue:
+        box = queue.popleft()
+        visited.add(box)
+
+        for key in boxes[box]:
+            if key not in visited and key < n:
+                queue.append(key)
+    return len(visited) == n
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,2 @@
+This is alx interview question 
+where by you determine which boxes were open
