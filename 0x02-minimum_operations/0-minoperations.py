d93542f589d0bc8cae3d7272db170bf302112adc
frankkib <frankkibagendi13@gmail.com>
2023-08-11 07:02:53 +0000

    determining the minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,9 +7,9 @@ def minOperations(n):
     """Return the number of opertaions exactly n"""
     if n == 1:
         return 0
-    dp = [0] * (n + 1)
+    dp = [0, 0]
     for i in range(2, n + 1):
-        dp[i] = float('inf')
+        dp.append(float('inf'))
         for j in range(1, i):
             if i % j == 0:
                 dp[i] = min(dp[i],  dp[j] + (i // j))
