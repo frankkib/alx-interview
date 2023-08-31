777c3d09551e8226e10453a2df2e55dcbadd3da1
frankkib <frankkibagendi13@gmail.com>
2023-08-31 11:37:47 +0000

    The N queens puzzle is the challenge of placing N non-attacking queens on an N×N chessboard

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+The N queen interview question
+"""
+
+
+def nqueens(n):
+    """
+    Solve the N queens puzzle and print solutions
+    """
+    board = [-1] * n
+    solve_nqueens(board, 0, n)
+
+
+def is_valid(board, row, col):
+    """
+    Check if it's safe to place a queen at the given row and column
+    """
+    for prev_row in range(row):
+        if (
+            board[prev_row] == col
+            or abs(board[prev_row] - col) == abs(prev_row - row)
+        ):
+            return False
+    return True
+
+
+def solve_nqueens(board, row, n):
+    """
+    Recursively solve the N queens puzzle by placing queens on the board.
+    """
+    if row == n:
+        print([board[i] for i in range(n)])
+        return
+
+    for col in range(n):
+        if is_valid(board, row, col):
+            board[row] = col
+            solve_nqueens(board, row + 1, n)
+
+
+if __name__ == "__main__":
+    nqueens(5)
