e0e0a5a3a9f8adef39daebdc796d97c62e0bbfb4
frankkib <frankkibagend13@gmail.com>
2023-10-30 06:27:34 +0000

    The prime winner game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,49 +1,39 @@
 #!/usr/bin/python3
 """
-The prime game interview question
+Prime interview question
 """
+
+
+def is_prime(num):
+    """
+    Function  for checking if the number is prime or not
+    """
+    if num <= 1:
+        return False
+    if num == 2:
+        return True
+    if num % 2 == 0:
+        return False
+
+    for i in range(3, int(num**0.5) + 1, 2):
+        if num % i == 0:
+            return False
+    return True
 
 
 def isWinner(x, nums):
     """
-    function that returns the prime winner
+    function for finding the winner
     """
-    def isPrime(num):
-        """
-        function that checks for prime numbers
-        """
-        if num < 2:
-            return False
-
-        for i in range(2, int(num**0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
-
-    dp = [None] * (max(nums) + 1)
-
-    for n in range(2, (max(nums) + 1)):
-        if dp[n] is not None:
-            continue
-        if isPrime(n):
-            dp[n] = "Maria"
-        else:
-            prime_factor = False
-            for i in range(2, n):
-                if n % i == 0 and dp[i] == "Ben":
-                    prime_factor = True
-                    dp[n] = "Maria"
-                    break
-            if not prime_factor:
-                dp[n] = "Ben"
     maria_wins = 0
     ben_wins = 0
 
     for n in nums:
-        if dp[n] == "Maria":
-            maria_wins += 1
-        elif dp[n] == "Ben":
+        prime_count = sum(1 for i in range(2, n + 1) if is_prime(i))
+        if prime_count % 2 == 0:
             ben_wins += 1
+        else:
+            maria_wins += 1
 
     if maria_wins > ben_wins:
         return "Maria"
