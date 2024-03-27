99a27efc4996a678eca62322f21b26e019540824
frankkib <frankkibagend13@gmail.com>
2024-03-27 07:39:13 +0000

    WOrking java-Scrip APIs for star wars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,43 +1,51 @@
 #!/usr/bin/node
 const request = require('request');
+const process = require('process');
 
-// Function to fetch characters for a given movie ID
-function getCharacters (movieId) {
-  const url = `https://swapi.dev/api/films/${movieId}/`;
-
-  request(url, (error, response, body) => {
-    if (error) {
-      console.error('Error:', error);
-      return;
-    }
-
-    if (response.statusCode !== 200) {
-      console.error('Status:', response.statusCode);
-      return;
-    }
-
-    const filmData = JSON.parse(body);
-    const characters = filmData.characters;
-
-    // Print each character name
-    characters.forEach(characterUrl => {
-      request(characterUrl, (error, response, body) => {
-        if (error) {
-          console.error('Error:', error);
-          return;
-        }
-
-        if (response.statusCode !== 200) {
-          console.error('Status:', response.statusCode);
-          return;
-        }
-
-        const characterData = JSON.parse(body);
-        console.log(characterData.name);
-      });
+function getMovieCharacters (movieId) {
+  return new Promise((resolve, reject) => {
+    const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+    request.get(url, (error, response, body) => {
+      if (error) {
+        reject(new Error(error));
+      } else if (response.statusCode !== 200) {
+        reject(new Error(`Failed to fetch movie data for movie ID ${movieId}`));
+      } else {
+        const movieData = JSON.parse(body);
+        const charactersUrls = movieData.characters;
+        const charactersNames = [];
+        let charactersCount = 0;
+        charactersUrls.forEach(characterUrl => {
+          request.get(characterUrl, (error, response, body) => {
+            if (!error && response.statusCode === 200) {
+              const characterData = JSON.parse(body);
+              charactersNames.push(characterData.name);
+              charactersCount++;
+              if (charactersCount === charactersUrls.length) {
+                resolve(charactersNames);
+              }
+            } else {
+              reject(new Error(`Failed to fetch character data for ${characterUrl}`));
+            }
+          });
+        });
+      }
     });
   });
 }
 
+if (process.argv.length !== 3) {
+  console.log('Usage: node script.js <movie_id>');
+  process.exit(1);
+}
+
 const movieId = process.argv[2];
-getCharacters(movieId);
+getMovieCharacters(movieId)
+  .then(characters => {
+    characters.forEach(character => {
+      console.log(character);
+    });
+  })
+  .catch(error => {
+    console.error(error);
+  });
